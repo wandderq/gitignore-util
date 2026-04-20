0c4abf0cfc838e144aafbd9626909a89d2d5157c
wandderq <wanddr.qq@gmail.com>
2026-04-20 18:38:23 +0300

    egiti.py splitted

--- src/egiti/cli.py
+++ src/egiti/cli.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 from egiti.template_loader import GITIGNORE_TEMPLATES, load_template
-from typing import Literal
-from pathlib import Path
+from egiti.core import GitignoreManager
 
 import colorlog as clg
 import logging as lg
@@ -27,74 +26,10 @@ stream_handler.setFormatter(clg.ColoredFormatter(
 root_logger.addHandler(stream_handler)
 
 
-class GitignoreManager:
-    def __init__(self, filepath: str) -> None:
-        self.logger = lg.getLogger('gitignore.manager')
-
-        self.path = Path(filepath)
-        if self.path.is_dir():
-            self.path /= '.gitignore'
-        self.path = self.path.absolute()
-
-        if not self.path.exists():
-            self.logger.warning(f'{self.path} not found')
-            create = not input('create it? (default: y) (y/n): ').strip().lower() in ['n', 'no']
-
-            if create:
-                self.path.parent.mkdir(parents=True, exist_ok=True)
-                self.path.touch()
-            
-            else:
-                self.logger.error(f'{filepath} not found')
-                sys.exit(1)
-        
-
-    def get_entries(self, with_comments: bool = False) -> list:
-        with open(self.path, 'r', encoding='utf-8') as file:
-            entries = []
-
-            for line in file:
-                line = line.strip()
-                if not line: continue
-
-                if with_comments or not line.startswith('#'):
-                    entries.append(line)
-
-        
-        return entries
-    
-
-    def add_entries(self, new_entries: list, mode: Literal['a', 'w'] = 'a') -> None:
-        with open(self.path, 'r', encoding='utf-8') as file:
-            raw_file_lines = file.readlines()
-            file_lines = [l.strip() for l in raw_file_lines]
-
-        with open(self.path, mode, encoding='utf-8') as file:
-            if mode == 'a' and raw_file_lines and not raw_file_lines[-1].endswith('\n'):
-                file.write('\n')
-            
-            for entry in new_entries:
-                if not entry in file_lines:
-                    file.write(entry + '\n')
-                    self.logger.debug(f'entry added: {entry}\\n')
-                
-                else:
-                    self.logger.warning(f'entry alreday exists: {entry}')
-    
-
-    def remove_entries(self, rm_entries: list) -> None:
-        old_entries = self.get_entries()
-        new_entries = [e for e in old_entries if not e in rm_entries]
-        self.logger.info(f'removed entries: {len(old_entries)-len(new_entries)}')
-
-        with open(self.path, 'w', encoding='utf-8') as file:
-            file.write('\n'.join(new_entries) + '\n')
-
-
-class GitignoreCLI:
+class EGITICLI:
     def __init__(self) -> None:
         self.argument_parser = ArgumentParser(
-            description='Quick editor for .gitignore files',
+            description='A utility for easily managing gitignore files ',
             epilog='GitHub: https://github.com/wandderq/gitignore-util'
         )
 
@@ -126,7 +61,9 @@ class GitignoreCLI:
 
     def run(self) -> None | int:
         args = self.argument_parser.parse_args()
-        if args.verbose: root_logger.setLevel(lg.DEBUG)
+        if args.verbose:
+            root_logger.setLevel(lg.DEBUG)
+        
         logger = lg.getLogger('gitignore.cli')
         
         manager = GitignoreManager(str(args.input_file).strip())
@@ -173,7 +110,7 @@ class GitignoreCLI:
 
 def run_cli():
     try:
-        app = GitignoreCLI()
+        app = EGITICLI()
         return app.run()
     
     except Exception as e:
---
+++ src/egiti/core.py
@@ -0,0 +1,69 @@
+from typing import Literal
+from pathlib import Path
+
+import logging as lg
+import sys
+
+
+class GitignoreManager:
+    def __init__(self, filepath: str) -> None:
+        self.logger = lg.getLogger('gitignore.manager')
+
+        self.path = Path(filepath)
+        if self.path.is_dir():
+            self.path /= '.gitignore'
+        self.path = self.path.absolute()
+
+        if not self.path.exists():
+            self.logger.warning(f'{self.path} not found')
+            create = not input('create it? (default: y) (y/n): ').strip().lower() in ['n', 'no']
+
+            if create:
+                self.path.parent.mkdir(parents=True, exist_ok=True)
+                self.path.touch()
+            
+            else:
+                self.logger.error(f'{filepath} not found')
+                sys.exit(1)
+        
+
+    def get_entries(self, with_comments: bool = False) -> list:
+        with open(self.path, 'r', encoding='utf-8') as file:
+            entries = []
+
+            for line in file:
+                line = line.strip()
+                if not line: continue
+
+                if with_comments or not line.startswith('#'):
+                    entries.append(line)
+
+        
+        return entries
+    
+
+    def add_entries(self, new_entries: list, mode: Literal['a', 'w'] = 'a') -> None:
+        with open(self.path, 'r', encoding='utf-8') as file:
+            raw_file_lines = file.readlines()
+            file_lines = [l.strip() for l in raw_file_lines]
+
+        with open(self.path, mode, encoding='utf-8') as file:
+            if mode == 'a' and raw_file_lines and not raw_file_lines[-1].endswith('\n'):
+                file.write('\n')
+            
+            for entry in new_entries:
+                if not entry in file_lines:
+                    file.write(entry + '\n')
+                    self.logger.debug(f'entry added: {entry}\\n')
+                
+                else:
+                    self.logger.warning(f'entry alreday exists: {entry}')
+    
+
+    def remove_entries(self, rm_entries: list) -> None:
+        old_entries = self.get_entries()
+        new_entries = [e for e in old_entries if not e in rm_entries]
+        self.logger.info(f'removed entries: {len(old_entries)-len(new_entries)}')
+
+        with open(self.path, 'w', encoding='utf-8') as file:
+            file.write('\n'.join(new_entries) + '\n')
